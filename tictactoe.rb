bd81c10e8d8efa64766386471b45e7a0a657ca3c
Oscar Daniels <oscardaniels@gmail.com>
2016-03-17 16:02:16 -0400

    refined winner? method

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,5 @@
 require "pry"
+# Tic Tac Toe Program - Oscar Daniels
 #Methods
 def draw_board(b)
   system "cls" #For Windows users
@@ -31,45 +32,10 @@ def player_pick(b)
   draw_board(b) 
 end
 
-def can_computer_win(b, winning_rows)
-  winning_rows.each do |winning_row|
-    values = b.values_at(winning_row[0], winning_row[1], winning_row[2])
-    if values.count("O") == 2 && values.count(" ") == 1
-      b[winning_row.index(" ")] = "O"
-      winning_row.each do |square| 
-        if b[square] == " "
-          b[square] = "O"   
-        end
-      end
-      draw_board(b)
-      return true
-    end
-  end
-  false
-end
-
-def can_computer_block(b, winning_rows)
-  winning_rows.each do |winning_row|
-    values = b.values_at(winning_row[0], winning_row[1], winning_row[2])
-    if values.count("X") == 2 && values.count(" ") == 1
-      b[winning_row.index(" ")] = "O"
-      winning_row.each do |square| 
-        if b[square] == " "
-          b[square] = "O"
-          binding.pry   
-        end
-      end
-      draw_board(b)
-      return true
-    end
-  end
-  false
-end
-
-def computer_pick(b, winning_rows)
-  if can_computer_win(b, winning_rows)
+def computer_pick(b)
+  if can_computer_win?(b)
     return
-  elsif can_computer_block(b, winning_rows)      
+  elsif can_computer_block?(b)      
     return
   else
     available_squares = find_available_squares(b) 
@@ -77,43 +43,69 @@ def computer_pick(b, winning_rows)
     b[choice] = "O"
     draw_board(b)
   end
-  
+end
+
+def can_computer_win?(b)
+  WINNING_ROWS.each do |winning_row|
+    values = b.values_at(winning_row[0], winning_row[1], winning_row[2])
+    if values.count("O") == 2 && values.count(" ") == 1
+      computer_move(b, winning_row)
+      return true
+    end
+  end
+  false
+end
+
+def can_computer_block?(b)
+  WINNING_ROWS.each do |winning_row|
+    values = b.values_at(winning_row[0], winning_row[1], winning_row[2])
+    if values.count("X") == 2 && values.count(" ") == 1
+      computer_move(b, winning_row)
+      return true
+    end
+  end
+  false
+end
+
+def computer_move(b, target_squares)
+  target_squares.each do |square| 
+    if b[square] == " "
+      b[square] = "O"
+      draw_board(b)  
+    end
+  end
 end   
 
-def winner?(b, player_piece, winning_rows)
+def winner?(b, player_piece)
   game_over = false 
-  winning_rows.each do |winning_row|
-    if b[winning_row[0]] == player_piece &&  
-       b[winning_row[1]] == player_piece &&  
-       b[winning_row[2]] == player_piece
+  WINNING_ROWS.each do |winning_row|
+    if winning_row.all? {|square| b[square] == player_piece}
      game_over = true
     end
   end
   return game_over
 end
 
-#variables
+#variables and constants
 board = {}
-winning_rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+WINNING_ROWS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 #main program
 initialize_board(board)
-binding.pry
 draw_board(board)
-
 begin  
   player_pick(board)
-  if winner?(board, "X", winning_rows)
+  if winner?(board, "X")
     puts "\nPlayer Wins!"
     break
   end
-  computer_pick(board, winning_rows) 
-  if winner?(board, "O", winning_rows)
+  computer_pick(board) 
+  if winner?(board, "O")
     puts "\nComputer Wins!"
     break
   end
 end until !board_full?(board)
 
-if !board_full?(board) && !winner?(board, "X", winning_rows) && !winner?(board, "O", winning_rows)
+if !board_full?(board) && !winner?(board, "X") && !winner?(board, "O")
   puts "It's a tie. All squares are gone."
 end
