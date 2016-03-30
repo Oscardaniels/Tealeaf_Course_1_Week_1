a2eaac02a8c7dfa71861e5e3a38fa99625a38e4e
Oscar Daniels <oscardaniels@gmail.com>
2016-03-30 16:21:55 -0400

    Saves players name feature"

--- blackjack.rb
+++ blackjack.rb
@@ -1,27 +1,16 @@
-#1 draw display
-#2 create a new deck, a hash of card value and points for that card
-#3 deal a card = move a random card from the deck into a player's hand
-  #3 and delete it from the deck)
-#4 deal player and dealer two cards each. 
-  #4 both player's cards are face up. dealer has one down 
-#4A player cards are summed and the sum is displayed. if they total 21, player wins.
-#5 if not, player chooses to hit or stay
-#6 if player chooses to hit they are dealt a new card
-#7 this continues until the player hits goes over 21 or chooses to stay
-#8 if computer has less than 17, the dealer must hit.
-#9 if the dealer hits 21, the dealer wins automatically.
-#10 if the dealer has more than 17, the dealer will stay and hands will be compared.
-require "pry"
-
-def say(msg)
+# blackjack - Oscar Daniels
+def say(msg, name)
   puts msg
 end
 
-def say_greeting
-  say "\n\nWelcome to Blackjack!"
+def say_greeting(name)
+  say("\n\nWelcome to Blackjack!", name)
   sleep 1
-  say "Shuffling the deck..."
-  sleep 1
+  say("What's your name?", name)
+  name = gets.chomp.capitalize
+  say("Hi #{name}. I'm shuffling the deck...", name)
+  sleep 2
+  name
 end
 
 def deal_card(current_deck, dealt_cards, whose_turn)
@@ -42,36 +31,24 @@ def update_totals(dealt_cards, total, whose_turn)
   dealt_cards[whose_turn].values.each do |card_value|
     total[whose_turn] = total[whose_turn] + card_value
   end
-  if total[whose_turn] > 21 && has_unconverted_ace?(dealt_cards, whose_turn)
-    binding.pry
+  if total[whose_turn] > 21 && unconverted_ace?(dealt_cards, whose_turn)
     ace_11_to_1_conversion(dealt_cards, whose_turn)
     update_totals(dealt_cards, total, whose_turn)    
   end
 end
 
-def has_unconverted_ace?(dealt_cards, whose_turn)
+def unconverted_ace?(dealt_cards, whose_turn)
   dealt_cards[whose_turn].has_value?(11)
 end
 
 def ace_11_to_1_conversion(dealt_cards, whose_turn)
   ace = dealt_cards[whose_turn].key(11)
   dealt_cards[whose_turn][ace] = 1    
-
-    # if index = (dealt_cards[whose_turn].has_key?(" AH") &&
-    #            dealt_cards[whose_turn][" AH"] == 11)
-    #   dealt_cards[whose_turn][" AH"] = 1
-    # elsif index = dealt_cards[whose_turn].has_key?(" AC")
-    #   dealt_cards[whose_turn][" AC"] = 1
-    # elsif index = dealt_cards[whose_turn].has_key?(" AD")
-    #   dealt_cards[whose_turn][" AD"] = 1
-    # elsif index = dealt_cards[whose_turn].has_key?(" AS")
-    #   dealt_cards[whose_turn][" AS"] = 1        
-    # end
 end  
 
-def determine_player_choice
+def hit_or_stay(name)
   begin
-  say "\nWould you like to hit or stay (h/s)"
+  say("\n#{name}, would you like to hit or stay (h/s)", name)
   choice = gets.chomp.downcase
   end until choice == 'h' or choice == 's'
   choice
@@ -100,44 +77,47 @@ def retrieve_hole_card(computers_hidden_card, dealt_cards)
   dealt_cards[:computer][computers_hidden_card] = DECK[computers_hidden_card]
 end
 
-def say_player_results(total)
-  say "\nPlayer stays at #{total[:player]}" if total[:player] < 21
-  say "\n21 will be tough to beat." if total[:player] == 21
-  say "\nYou have #{total[:player]}." if total[:player] > 21
+def say_player_results(total, name)
+  say("\n#{name} stays at #{total[:player]}", name) if total[:player] < 21
+  say("\n21 will be tough to beat.", name) if total[:player] == 21
+  say("\nYou have #{total[:player]}.", name) if total[:player] > 21
 end
 
-def say_computer_results(total)
-  say "\nComputer wins!" if total[:computer] > total[:player] && 
-                            total[:computer] <= 21
-  say "\nIt's a push." if total[:computer] == total[:player]
-  say "\nComputer busted with #{total[:computer]}." if total[:computer] > 21
+def say_computer_results(total, name)
+  say("\nComputer wins!", name) if total[:computer] > total[:player] && 
+                                   total[:computer] <= 21
+  say("\nIt's a push.", name) if total[:computer] == total[:player]
+  say("\nComputer busted with #{total[:computer]}.", name) if total[:computer] > 21
   sleep 1
 end
 
 DECK = {
-          " AS" => 11,  
-          " AC" => 11, 
-          " AH" => 11,  
-          " AD" => 11, " 9D" => 9, "10D" => 10,
+          " AS" => 11,  " 2S" => 2,  " 3S" => 3, " 4S" => 4, " 5S" => 5, 
+          " 6S" => 6,  " 7S" => 7,  " 8S" => 8, " 9S" => 9, "10S" => 10,
+          " JS" => 10, " QS" => 10, " KS" => 10, 
+          " AC" => 11,  " 2C" => 2,  " 3C" => 3, " 4C" => 4, " 5C" => 5, 
+          " 6C" => 6,  " 7C" => 7,  " 8C" => 8, " 9C" => 9, "10C" => 10,
+          " JC" => 10, " QC" => 10, " KC" => 10,
+          " AH" => 11,  " 2H" => 2,  " 3H" => 3, " 4H" => 4, " 5H" => 5, 
+          " 6H" => 6,  " 7H" => 7,  " 8H" => 8, " 9H" => 9, "10H" => 10,
+          " JH" => 10, " QH" => 10, " KH" => 10,
+          " AD" => 11,  " 2D" => 2,  " 3D" => 3, " 4D" => 4, " 5D" => 5, 
+          " 6D" => 6,  " 7D" => 7,  " 8D" => 8, " 9D" => 9, "10D" => 10,
           " JD" => 10, " QD" => 10, " KD" => 10    
-
         }
-# need to handle ace 1 or 11
-say_greeting
-
+name = ""
+name = say_greeting(name) 
 loop do
   dealt_cards = {player: {}, computer: {}}
   total = {player: 0, computer: 0}
   whose_turn = :player
   current_deck = DECK.dup
-
   initial_deal(current_deck, dealt_cards)
   update_totals(dealt_cards,total, whose_turn)
   computers_hidden_card = hide_hole_card_from_display(dealt_cards)
   display_table(dealt_cards, total, whose_turn)
-
   while total[:player] < 21
-    if determine_player_choice == "h"
+    if hit_or_stay(name) == "h"
       deal_card(current_deck,dealt_cards, whose_turn)
       update_totals(dealt_cards, total, whose_turn)
       display_table(dealt_cards, total, whose_turn)
@@ -145,22 +125,22 @@ loop do
       break
     end
   end
-  say_player_results(total)
+  say_player_results(total, name)
   sleep 1
   if total[:player] > 21
-    say "You busted!" 
-  elsif total[:player] <= 21 || total[:player] < total[:computer]
+    say("You busted!", name) 
+  else 
     whose_turn = :computer
     retrieve_hole_card(computers_hidden_card, dealt_cards)
     update_totals(dealt_cards, total, whose_turn)
-    say "Computer's hole card is a #{computers_hidden_card}" + 
-        " for a total of #{total[:computer]}"
+    say("Computer's hole card is a #{computers_hidden_card}" + 
+        " for a total of #{total[:computer]}", name)
     sleep 3
     system "cls"
     display_table(dealt_cards, total, whose_turn)
     sleep 2
     while total[:computer] < total[:player] || total[:computer] < 17
-      say "\nComputer hits."
+      say("\nComputer hits.", name)
       sleep 1
       deal_card(current_deck, dealt_cards, whose_turn)
       update_totals(dealt_cards, total, whose_turn)
@@ -168,7 +148,7 @@ loop do
       sleep 1     
     end   
   end
-  say_computer_results(total) 
-  say "\nWould you like to play again? (y/n)"
+  say_computer_results(total, name) 
+  say("\n#{name}, would you like to play again? (y/n)", name)
   exit if gets.chomp.downcase != "y"
 end
